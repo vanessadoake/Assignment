dd56b9ade281a95cf11c98ac415f7aea597f4913
vanessadoake <vanessa@codelikeagirl.com.au>
2017-07-06 19:50:27 +1000

    Week 4 Assignment

---
+++ Week4Assignment/main.js
@@ -0,0 +1,21 @@
+// document.getElementsByTagName('h1')
+// $('h1').text()
+$(document).ready(toDoWhenReady);
+
+function toDoWhenReady() {
+  $('.readmore').click(showMore);
+  $('.readless').click(showLessContent);
+}
+  function showMore(event) {
+    event.preventDefault();
+    $('#show-this-on-click').slideDown();
+    $('.readmore').hide();
+    $('.readless').show();
+
+  }
+
+    function showLessContent(event) {
+      event.preventDefault();
+      $('#show-this-on-click').slideUp();
+
+}
